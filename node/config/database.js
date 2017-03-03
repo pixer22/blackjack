7cc84db77ffb43e5a91f702c9f25ee820ad0c632
pixer22 <pixer@list.ru>
2017-03-03 10:09:57 +0200

    datadbase mlab connect

--- node/config/database.js
+++ node/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/poker' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://pixer22:mh3486DA@ds113680.mlab.com:13680/blackjack' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
